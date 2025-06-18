bd079f89da35d2c7a23689c3710ccbf8428e25c7
Juan Tobias De La Cruz Acosta <juantobiasdl7@gmail.com>
2025-06-17 23:41:50 -0600

    Added a new structure to the nav bar.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Tobías De La Cruz Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
