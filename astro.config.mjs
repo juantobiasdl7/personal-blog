bff10b3ea28edfe879730b552f45022367d5fdf1
Juan Tobias De La Cruz Acosta <juantobiasdl7@gmail.com>
2025-06-21 00:50:22 -0600

    Fixed base URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://juantobiasdl7.github.io',
-	base: 'personal-blog',
+	base: 'personal-blog/',
 });
